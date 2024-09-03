e7ca7bd813c30862e9118a5d732c77be3f44c862
AramisB <brendachem7@gmail.com>
2024-09-03 16:33:50 +0300

    routes that do not need authenticating

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,32 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Returns false
+        Returns True if path is None
+        Returns True if excluded_paths is None or empty
+        Returns False if path is in excluded_paths
+        assume excluded_paths contains string path always ending by a /
+        This method must be slash tolerant:
+        path=/api/v1/status and path=/api/v1/status/
+        must be returned False if excluded_paths contains /api/v1/status/
         """
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            # Normalize the excluded_pathgit 
+            if not excluded_path.endswith('/'):
+                excluded_path += '/'
+
+            # Check if the path starts with the excluded path
+            if path.startswith(excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
