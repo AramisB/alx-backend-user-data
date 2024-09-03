5ec4d4ac2a042d5e8fe555828f6055a67e547ad8
AramisB <brendachem7@gmail.com>
2024-09-03 14:32:19 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized():
+    """
+    Endpoint to test 401 Unauthorized error.
+    """
+    abort(401)
