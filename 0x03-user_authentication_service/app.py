aac9f9cef0cfc1605d2d916ac32f853159ff24c1
AramisB <brendachem7@gmail.com>
2024-09-09 16:23:25 +0300

    implement the login function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,6 +4,7 @@ App module
 """
 from flask import app, jsonify, Flask, request
 from auth import Auth
+import flask
 
 
 app = Flask(__name__)
@@ -54,6 +55,43 @@ def users() -> None:
             "message": "email already registered"
         }), 400
 
+@app.route('/sessions', methods=['POST'])
+def login() -> None:
+    """
+    a function to respond to the POST /sessions route.
+    The request is expected to contain form data
+    with "email" and a "password" fields.
+    If the login information is incorrect,
+    use flask.abort to respond with a 401 HTTP status.
+    Otherwise, create a new session for the user,
+    store it the session ID as a cookie with key "session_id"
+    on the response and return a JSON payload of the form:
+    {"email": "<user email>", "message": "logged in"}
+    """
+    data = request.form
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return jsonify({
+            "message": "email and password are required"
+            }), 400
+
+    try:
+        user = AUTH.valid_login(email, password)
+        if user:
+            session_id = AUTH.create_session(user.email)
+        else:
+            flask.abort(401)
+        response = jsonify({
+            "email": user.email,
+            "message": "logged in"
+        })
+        response.set_cookie("session_id", session_id)
+        return response
+    except ValueError:
+        flask.abort(401)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
