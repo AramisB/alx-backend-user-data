c42375b7f0431559c8fe466f7936a9e41d9a6a5f
AramisB <brendachem7@gmail.com>
2024-09-11 12:06:21 +0300

    return User object not None

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,9 +34,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> None:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Add a new user to the database
+        Add a new user to the database and return the User object
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
